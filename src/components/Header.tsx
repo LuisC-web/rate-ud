e349d5127677d7058d9ac03b7fa5304a017b899b
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-28 08:43:08 -0500

    feat: change color when i will stay in route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,49 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Header() {
+  const pathname = usePathname();
+
   return (
-    <nav className="bg-primary text-secondary flex w-screen justify-between px-10 py-6 text-xl">
+    <nav className="bg-primary text-secondary flex w-screen justify-between px-10 py-6 text-xl transition-colors">
       <div className="flex gap-3">
-        <Link href={"/"}>Busca un profe</Link>
-        <Link href={"/new/teacher"}>Crea un profe</Link>
+        <Link
+          href="/"
+          className={`hover:text-primary-light cursor-pointer ${
+            pathname === "/" ? "text-primary-light" : ""
+          }`}
+        >
+          Busca un profe
+        </Link>
+        <Link
+          href="/new/teacher"
+          className={`hover:text-primary-light cursor-pointer ${
+            pathname === "/new/teacher" ? "text-primary-light" : ""
+          }`}
+        >
+          Crea un profe
+        </Link>
       </div>
       <div className="flex gap-3">
-        <Link href={"/new/review"}>Sube una reseña</Link>
-        <Link href={"/top"}>Top profes</Link>
+        <Link
+          href="/new/review"
+          className={`hover:text-primary-light cursor-pointer ${
+            pathname === "/new/review" ? "text-primary-light" : ""
+          }`}
+        >
+          Sube una reseña
+        </Link>
+        <Link
+          href="/top"
+          className={`hover:text-primary-light cursor-pointer ${
+            pathname === "/top" ? "text-primary-light" : ""
+          }`}
+        >
+          Top profes
+        </Link>
       </div>
     </nav>
   );
