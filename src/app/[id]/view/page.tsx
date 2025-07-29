9ff70f3563c00a625f07a3e87e737cd3b18988c3
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-29 08:15:53 -0500

    fix: implement view reviews

--- src/app/[id]/view/page.tsx
+++ src/app/[id]/view/page.tsx
@@ -1,8 +1,32 @@
-import Working from "@/components/ui/Working";
+import { redirect } from "next/navigation";
 import React from "react";
+import { getReviews } from "../../../../actions/get-reviews";
+import ReviewCard from "@/components/ui/ReviewCard";
 
-function ViewTeacher() {
-  return <Working></Working>;
+async function ViewTeacher({ params }: { params: Promise<{ id: string }> }) {
+  const id = +(await params).id;
+  if (!id) redirect("/");
+  const getReviewForTeacherId = await getReviews(id);
+
+  return (
+    <div className="mt-10 px-15">
+      <h1 className="text-4xl">
+        Referencia de {getReviewForTeacherId.data.name}, Carrera:
+        {getReviewForTeacherId.data.career.name}
+      </h1>
+      {getReviewForTeacherId.error.error ? (
+        <p className="bg-error text-2xl">
+          {getReviewForTeacherId.error.message}
+        </p>
+      ) : (
+        <div className="mt-10 grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {getReviewForTeacherId.data.reviews.map((review) => (
+            <ReviewCard key={review.id} review={review}></ReviewCard>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ViewTeacher;
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,4 +1,10 @@
 import { Prisma } from "@prisma/client";
+export type ReviewsType = Prisma.TeacherGetPayload<{
+  include: { reviews: { include: { score: true } }; career: true };
+}>;
+export type ReviewType = Prisma.ReviewGetPayload<{
+  include: { score: true };
+}>;
 
 export type Teacher = Prisma.TeacherGetPayload<{
   include: { reviews: true; career: true };
