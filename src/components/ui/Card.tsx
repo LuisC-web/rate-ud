f7929ebd06b2ae2c6dc10e1c293d887ef71a2302
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-28 19:36:15 -0500

    feat:implement top teachers

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,27 +3,55 @@ import { redirect } from "next/navigation";
 
 type CardProps = {
   teacher: Teacher;
+  mobile?: boolean;
+  link?: boolean;
+  feedback?: boolean;
+  comments?: { autor: string; comment: string }[];
 };
-function Card({ teacher }: CardProps) {
+function Card({
+  teacher,
+  mobile = true,
+  link = true,
+  feedback = false,
+  comments,
+}: CardProps) {
   return (
     <div
-      className="bg-primary active:bg-primary/80 flex w-full flex-col rounded-xl p-4 text-white md:hidden"
-      onClick={() => redirect("/" + teacher.id + "/view")}
+      className={`bg-primary active:bg-primary/80 w-full rounded-xl p-4 text-white ${mobile ? "md:hidden" : ""} ${feedback ? "grid grid-cols-1 md:grid-cols-2" : "flex"}`}
+      onClick={() => {
+        redirect("/" + teacher.id + "/view");
+      }}
     >
-      <p className="flex items-center gap-2 text-base">
-        Nombre:
-        <span className="text-primary-light text-sm">{teacher.name}</span>
-      </p>
-      <p className="flex items-center gap-2 text-base">
-        Carrera:
-        <span className="text-primary-light text-sm">
-          {teacher.career.name}
-        </span>
-      </p>
-      <p className="flex items-center gap-2 text-base">
-        Calificacion:
-        <span className="text-primary-light text-sm">{teacher.avg}</span>
-      </p>
+      <div className="w-full rounded-xl p-4">
+        <h1>Datos</h1>
+        <p className="flex items-center gap-2 text-base">
+          Nombre:
+          <span className="text-primary-light text-sm">{teacher.name}</span>
+        </p>
+        <p className="flex items-center gap-2 text-base">
+          Carrera:
+          <span className="text-primary-light text-sm">
+            {teacher.career.name}
+          </span>
+        </p>
+        <p className="flex items-center gap-2 text-base">
+          Calificacion:
+          <span className="text-primary-light text-sm">{teacher.avg}</span>
+        </p>
+      </div>
+      <div
+        className={`w-full rounded-xl p-4 text-white ${feedback ? "" : "hidden"}`}
+      >
+        <h1>Algunos comentarios</h1>
+        {comments?.map((comment) => (
+          <p className="flex items-center gap-2 text-base">
+            {comment.autor}:
+            <span className="text-primary-light text-sm">
+              {comment.comment}
+            </span>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
